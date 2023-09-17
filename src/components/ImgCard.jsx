1a969774181decc40184544ec7629b3b70a8d1f3
Omar Ahmed Saeed <85828826+0omarahmed73@users.noreply.github.com>
2023-09-17 22:04:56 +0300

    Update ImgCard.jsx

--- src/components/ImgCard.jsx
+++ src/components/ImgCard.jsx
@@ -1,17 +1,30 @@
 import React, { useContext } from "react";
 import { Col } from "react-bootstrap";
 import { ImgsContext } from "../context/ImgsContext";
+import LazyLoad from "react-lazy-load";
 
 const ImgCard = ({ d }) => {
   const { handleFlip } = useContext(ImgsContext);
   return (
     <>
-      <Col className="m-2" xs='5' sm='4' md='3' lg='2' onClick={() => handleFlip(d.id)} key={crypto.randomUUID()}>
+      <Col
+        className="m-2"
+        xs="5"
+        sm="4"
+        md="3"
+        lg="2"
+        onClick={() => handleFlip(d.id)}
+        key={crypto.randomUUID()}
+      >
         <div className="img">
           {d.flip ? (
-            <img src={d.img} alt="" />
+            <LazyLoad>
+              <img src={d.img} alt="" />
+            </LazyLoad>
           ) : (
-            <img src="https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg" />
+            <LazyLoad height="100%">
+              <img src="https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg" />
+            </LazyLoad>
           )}
         </div>
       </Col>
